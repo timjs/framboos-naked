8da8b666e2c3282b2bc05047bc9d1ba2412cb1a7
Tim Steenvoorden <tim.steenvoorden@gmail.com>
2022-11-26 19:56:52 +0100

    fix naming of graphic functions

--- src/shared/graphics.c
+++ src/shared/graphics.c
@@ -5,24 +5,29 @@
 #include "kernel/panic.h"
 #include "shared/bitfont.h"
 
-void draw_pixel(framebuffer_info_t *fb, point_t point, color_t color) {
-  fb->buf[point.x * BYTES_PER_PIXEL + point.y * fb->pitch + 2] = color.red;
-  fb->buf[point.x * BYTES_PER_PIXEL + point.y * fb->pitch + 1] = color.green;
-  fb->buf[point.x * BYTES_PER_PIXEL + point.y * fb->pitch + 0] = color.blue;
+void draw_pixel(framebuffer_info_t *fb, point_t position, color_t color) {
+  fb->buf[position.x * BYTES_PER_PIXEL + position.y * fb->pitch + 2] =
+      color.red;
+  fb->buf[position.x * BYTES_PER_PIXEL + position.y * fb->pitch + 1] =
+      color.green;
+  fb->buf[position.x * BYTES_PER_PIXEL + position.y * fb->pitch + 0] =
+      color.blue;
 }
 
 void draw_rectangle(framebuffer_info_t *fb, area_t area, color_t color) {
   kernel_panic("No implementation of `draw_rectangle");
 }
 
-void draw_character(framebuffer_info_t *fb, point_t pos, color_t col, char c) {
+void draw_character(framebuffer_info_t *fb, point_t position, color_t color,
+                    char c) {
   const uint8_t *data = font(c);
 
   for (int y = 0; y < 8; y++) {
     uint8_t line = data[y];
     for (int x = 0; x < 8; x++) {
       if ((line & 1) != 0) {
-        draw_pixel(fb, (point_t){.x = pos.x + x, .y = pos.y + y}, col);
+        draw_pixel(fb, (point_t){.x = position.x + x, .y = position.y + y},
+                   color);
       }
 
       line >>= 1;
@@ -30,6 +35,7 @@ void draw_character(framebuffer_info_t *fb, point_t pos, color_t col, char c) {
   }
 }
 
-void draw_string(framebuffer_info_t *fb, point_t pos, color_t col, char *str) {
+void draw_string(framebuffer_info_t *fb, point_t position, color_t color,
+                 char *str) {
   kernel_panic("No implementation of `draw_string`")
 }
